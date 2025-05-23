65ce6cfef322dc2d4068c8096ed28ef704c34e15
Happy2018new <chenbo79800@163.com>
2025-05-23 13:54:34 +0800

    Test round 9

--- test2.py
+++ test2.py
@@ -1,52 +1,59 @@
-import ctypes
-import struct
+# import ctypes
+# import struct
 
-CPtr = ctypes.c_void_p
-CSlice = CPtr
-CString = CPtr
-CInt = ctypes.c_int
-CLongLong = ctypes.c_longlong
+# CPtr = ctypes.c_void_p
+# CSlice = CPtr
+# CString = CPtr
+# CInt = ctypes.c_int
+# CLongLong = ctypes.c_longlong
 
-LIB = ctypes.cdll.LoadLibrary("./c_api_test.so")
-LIB.FreeMemory.argtypes = [CPtr]
-LIB.FreeMemory.restype = None
-
-
-def free_memory(address: CPtr) -> None:
-    LIB.FreeMemory(address)
+# LIB = ctypes.cdll.LoadLibrary("./c_api_test.so")
+# LIB.FreeMemory.argtypes = [CPtr]
+# LIB.FreeMemory.restype = None
 
 
-def as_c_bytes(b: bytes) -> CSlice:
-    return ctypes.cast(ctypes.c_char_p(struct.pack("<I", len(b)) + b), CSlice)
+# def free_memory(address: CPtr) -> None:
+#     LIB.FreeMemory(address)
 
 
-def as_python_bytes(slice: CSlice) -> bytes:
-    length = struct.unpack("<I", ctypes.string_at(slice, 4))[0]
-    result = ctypes.string_at(slice, 4 + length)[4:]
-    free_memory(slice)
-    return result
+# def as_c_bytes(b: bytes) -> CSlice:
+#     return ctypes.cast(ctypes.c_char_p(struct.pack("<I", len(b)) + b), CSlice)
 
 
-def as_c_string(string: str) -> CString:
-    return ctypes.cast(ctypes.c_char_p(bytes(string, encoding="utf-8")), CString)
+# def as_python_bytes(slice: CSlice) -> bytes:
+#     length = struct.unpack("<I", ctypes.string_at(slice, 4))[0]
+#     result = ctypes.string_at(slice, 4 + length)[4:]
+#     free_memory(slice)
+#     return result
 
 
-def as_python_string(c_string: CString) -> str:
-    result = ctypes.c_char_p(c_string).value.decode(encoding="utf-8")  # type: ignore
-    free_memory(c_string)
-    return result
+# def as_c_string(string: str) -> CString:
+#     return ctypes.cast(ctypes.c_char_p(bytes(string, encoding="utf-8")), CString)
 
 
-LIB.DO.argtypes = []
-LIB.DO2.argtypes = []
-LIB.DO.restype = None
-LIB.DO2.restype = None
+# def as_python_string(c_string: CString) -> str:
+#     result = ctypes.c_char_p(c_string).value.decode(encoding="utf-8")  # type: ignore
+#     free_memory(c_string)
+#     return result
 
-LIB.NewTimelineDB.argtypes = [CString, CInt, CInt]
-LIB.NewTimelineDB.restype = CLongLong
 
-# LIB.DO()
-# print("OK")
+# LIB.DO.argtypes = []
+# LIB.DO2.argtypes = []
+# LIB.DO.restype = None
+# LIB.DO2.restype = None
 
-print(int(LIB.NewTimelineDB(as_c_string("ssss"), CInt(False), CInt(False))))
-print("OK2")
+# LIB.NewTimelineDB.argtypes = [CString, CInt, CInt]
+# LIB.NewTimelineDB.restype = CLongLong
+
+# # LIB.DO()
+# # print("OK")
+
+# print(int(LIB.NewTimelineDB(as_c_string("ssss"), CInt(False), CInt(False))))
+# print("OK2")
+
+import time
+from python.package.internal.symbol_export_timeline_db import new_timeline_db
+
+print(new_timeline_db("why", False, False))
+time.sleep(1)
+print("OK")
